a33194e33bf3050a67130b0cd2fc6317c08bc98e
Khristopher <khristopheroficial@gmail.com>
2025-03-20 19:00:34 -0400

    atualizacao contatos

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,31 +4,22 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
-import { 
-    Select, 
-    SelectContent, 
-    SelectGroup, 
-    SelectItem, 
-    SelectLabel, 
-    SelectTrigger, 
-    SelectValue 
-} from "@/components/ui/select";
 
 import { FaEnvelope, FaLinkedin, FaDiscord } from 'react-icons/fa';
 
 const info = [
     {
-        icon: <FaEnvelope/>,
+        icon: <FaEnvelope />,
         title: 'Email',
         description: 'khristopheroficial@gmail.com'
     },
     {
-        icon: <FaDiscord/>,
+        icon: <FaDiscord />,
         title: 'Discord',
         description: 'khristopher_kidman'
     },
     {
-        icon: <FaLinkedin/>,
+        icon: <FaLinkedin />,
         title: 'Linkedin',
         description: 'Khristopher Kidman',
     }
@@ -37,18 +28,18 @@ const info = [
 import { motion } from "framer-motion";
 
 const Contact = () => {
-    return(
-        <motion.section 
-            initial={{opacity: 0}} 
-            animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: 'easeIn'}}} 
+    return (
+        <motion.section
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' } }}
             className='py-6'
         >
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]">
 
                     <div className="xl:w-[54%] order-2 xl:order-none">
-                        <form 
-                            action="https://formsubmit.co/khristopheroficial@gmail.com" 
+                        <form
+                            action="https://formsubmit.co/khristopheroficial@gmail.com"
                             method="POST"
                             className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
                         >
@@ -64,10 +55,10 @@ const Contact = () => {
                                 <Input type="phone" placeholder="Phone" />
                             </div>
 
-                            <Textarea 
+                            <Textarea
                                 name="message"
                                 className="h-[200px]"
-                                placeholder="Type your message here." 
+                                placeholder="Type your message here."
                                 required
                             />
 
@@ -81,7 +72,7 @@ const Contact = () => {
                     <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
                         <ul className="flex flex-col gap-10">
                             {info.map((item, index) => {
-                                return(
+                                return (
                                     <li key={index} className="flex items-center gap-6">
                                         <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rouded-md flex items-center justify-center">
                                             <div className="text-[28px]">{item.icon}</div>
