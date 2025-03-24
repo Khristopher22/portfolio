b54a9c6a23a3d326380562a156b097ce71e2387c
Khristopher <khristopheroficial@gmail.com>
2025-03-24 18:48:52 -0400

    atualizacao page

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,11 @@ const Home = () => {
               <Button asChild variant="outline" size="lg" className="uppercase flex items-center gap-2">
                 <a href="/assets/curriculum/curriculum.pdf" download="curriculum.pdf">
                   <span>Download CV</span>
-                  <FiDownload className="text-xl"/>
+                  <FiDownload className="text-xl" />
                 </a>
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Social  containerStyles="flex gap-6"  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
